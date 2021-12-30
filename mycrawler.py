33c2d9c69061611fbf8a6f4f670d7da922484ce1
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2021-12-30 19:19:18 +0800

    Update mycrawler.py

--- mycrawler.py
+++ mycrawler.py
@@ -10,7 +10,7 @@ import time
 class MyCrawler:
 	HEADERS = {
 		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0."
-		"4664.55 Safari/537.36",
+		"4664.55 Safari/537.36"
 	}  # 请求头
 
 	def __init__(self, cookie="", encoding="utf-8"):
