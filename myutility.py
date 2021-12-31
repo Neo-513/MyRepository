674b42a11869bfebe2471e8736fa4f28d3990c10
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2021-12-31 13:31:09 +0800

    Update myutility.py

--- myutility.py
+++ myutility.py
@@ -5,50 +5,38 @@ import jieba
 import json
 import time
 
-super_path = "D:/MyCodes"  # 主目录
 
+def read(path):  # 读文件
+	print(f"READ: {path}")
+	file_type = path.split(".")[-1]  # 文件类型
+	encoding = "gbk" if file_type == "csv" else "utf-8"  # 编码
 
-def read(path, show_len=False, split=True):  # 读文件
-	if "." not in path:
-		path = f"{super_path}/myfiles/{path}.csv"
-
-	filetype = path.split(".")[-1]
-	if filetype == "csv":
-		with open(path, mode="r", encoding="gbk") as f:
+	with open(path, mode="r", encoding=encoding) as f:
+		if file_type == "txt":
+			datas = f.read().split("\n")
+		elif file_type == "csv":
 			datas = [[r for r in reader] for reader in csv.reader(f)]
 			if len(datas[0]) == 1:  # 一维列表
 				datas = [data[0] for data in datas]
-	elif filetype == "json":
-		with open(path, mode="r", encoding="utf-8") as f:
+		elif file_type == "json":
 			datas = demjson.decode(f.read())
-	elif filetype == "txt":
-		with open(path, mode="r", encoding="utf-8") as f:
-			datas = f.read()
-			if show_len:
-				print(f"[{len(datas)}]", end="    ")
-			if split:
-				datas = datas.split("\n")
-	print(f"READ: {path}")
 	return datas
 
 
 def write(path, datas):  # 写文件
-	if "." not in path:
-		path = f"{super_path}/myfiles/{path}.csv"
+	print(f"WRITE: {path}")
+	file_type = path.split(".")[-1]  # 文件类型
+	encoding = "gbk" if file_type == "csv" else "utf-8"  # 编码
 
-	filetype = path.split(".")[-1]
-	if filetype == "csv":
-		with open(path, mode="w", encoding="gbk", newline="") as f:
+	with open(path, mode="w", encoding=encoding, newline="") as f:
+		if file_type == "txt":
+			f.write(datas)
+		elif file_type == "csv":
 			if not isinstance(datas[0], list):  # 一维列表
 				datas = [[data] for data in datas]
 			csv.writer(f).writerows(datas)
-	elif filetype == "json":
-		with open(path, mode="w", encoding="utf-8") as f:
+		elif file_type == "json":
 			f.write(demjson.encode(datas))
-	elif filetype == "txt":
-		with open(path, mode="w", encoding="utf-8") as f:
-			f.write(datas)
-	print(f"WRITE: {path}")
 
 
 def mysort(items, idx=0, reverse=True):  # 排序列表和字典
@@ -64,23 +52,24 @@ def lst2dic(lst):  # 统计列表元素个数
 	return mysort(dic, 1)  # 对字典按值排序
 
 
-def jsn2dic(jsn):  # json字符串转为字典
+def str2dic(string):  # 字符串转为字典
 	dic = {"null": "None", "true": "True", "false": "False"}
 	for d in dic:
-		jsn = jsn.replace(d, dic[d])
-	return eval(jsn)
+		string = string.replace(d, dic[d])
+	return eval(string)
 
 
-def beautify_jsn(jsn):  # 格式化打印json字符串
-	if isinstance(jsn, str):
-		jsn = jsn2dic(jsn)
-	print(json.dumps(jsn, indent=4, ensure_ascii=False))
+def beautify_dic(dic):  # 格式化打印字典
+	if isinstance(dic, str):
+		dic = str2dic(dic)
+	print(json.dumps(dic, indent=4, ensure_ascii=False))
 
 
-def myprint(items):  # 打印
+def myprint(items):  # 迭代打印
 	if isinstance(items, dict):
+		max_len = max(map(lambda x: len(str(x)), items))  # 列表元素最大长度
 		for k, v in items.items():
-			print(f"{k}    {v}")
+			print(f"{k:<{max_len + 4}}{v}")
 	else:
 		for item in items:
 			print(item)
@@ -98,48 +87,3 @@ def timing(func):  # 统计时间
 def analyze_nlp(kws):  # 中文分词
 	keywords = sum(map(lambda x: jieba.lcut(x), kws), [])
 	return lst2dic(keywords)
-
-
-'''
-for i in range(timeout * 2 + 1):  # 显示进度条
-	print(f"\rLOADING:{(i / (timeout * 2)) * 100:>6.2f}% {'■' * i}{'□' * (timeout * 2 - i)}", end="")
-	time.sleep(0.5)
-
-
-# import win32api
-# import win32con
-
-
-def remove_repeat(folder_src, folder_dst):  # 文件去重
-	files_src = []  # 不变
-	for folder in os.listdir(folder_src):
-		files_src += os.listdir("%s/%s" % (folder_src, folder))
-	files_dst = os.listdir(folder_dst)  # 变
-
-	for file_dst in files_dst:
-		if file_dst in files_src:
-			src = "%s/%s" % (folder_dst, file_dst)
-			dst = "E:/recycle/" + file_dst
-			os.rename(src, dst)
-			print("REPEAT: " + file_dst)
-		else:
-			print("NOT REPEAT: " + file_dst)
-
-def click(x, y, interval=0.3):
-	win32api.SetCursorPos([x, y])
-	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-	time.sleep(interval)
-
-
-def keyboard(key):
-	win32api.keybd_event(key, 0, 0, 0)
-	win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
-
-
-def normalize_symbol(data):
-	for i in range(len(data)):
-		d = data[i]
-		if 65296 <= ord(d) <= 65305 or 65313 <= ord(d) <= 65339 or 65345 <= ord(d) <= 65371:
-			data = data.replace(d, chr(ord(d) - 65248))
-	return data
-'''
