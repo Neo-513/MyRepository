c054ba7f3927646eea5a85d697ba8c65d913b47c
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-01-14 12:19:50 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -14,7 +14,7 @@ class BiliBili:
 		"""
 		urls = [f"http://api.bilibili.com/x/v3/fav/resource/list?ps=20&media_id={fid}&pn={i + 1}" for i in range(75)]
 		datas = self.mc.crawl(urls)
-		items = map(lambda x: x["data"]["medias"], map(mu.str2dic, datas))
+		items = map(lambda x: x["data"]["medias"], map(mu.jsn2dic, datas))
 		return [[i["bv_id"], i["title"], i["upper"]["mid"], i["upper"]["name"]] for item in items if item for i in item]
 
 	def get_ups(self, tid=None):
@@ -25,5 +25,5 @@ class BiliBili:
 		"""
 		urls = [f"http://api.bilibili.com/x/relation/followings?vmid=37928647&order=desc&pn={i + 1}" for i in range(40)]
 		datas = self.mc.crawl(urls)
-		items = map(lambda x: x["data"]["list"], map(mu.str2dic, datas))
+		items = map(lambda x: x["data"]["list"], map(mu.jsn2dic, datas))
 		return {i["mid"]: i["uname"] for item in items for i in item if not tid or (i["tag"] and i["tag"][0] == tid)}
