df4105a3d8bb8bf8e2d71ad1d8f667f0b4effd82
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-01-14 12:21:23 +0800

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -1,6 +1,7 @@
 import collections
 import jieba
 import json
+import threading
 import time
 
 
@@ -19,27 +20,7 @@ def myprint(items):
 			print(item)
 
 
-def timing(func):
-	"""
-	统计函数执行时间（装饰器）
-	:param func: 待执行函数
-	:return: 回调函数
-	"""
-	def callback(*args, **kwargs):
-		"""
-		回调函数
-		:param args: 传递参数
-		:param kwargs: 传递参数
-		:return: 回调函数返回值
-		"""
-		timer = time.time()  # 计时器
-		datas = func(*args, **kwargs)  # 执行函数
-		print(f"TIMING: {time.time() - timer}")
-		return datas
-	return callback
-
-
-def mysort(items, idx=0, reverse=True):
+def mysort(items, idx, reverse=True):
 	"""
 	排序集合
 	:param items: 待排序集合
@@ -49,7 +30,6 @@ def mysort(items, idx=0, reverse=True):
 	"""
 	if isinstance(items, list):
 		items.sort(key=lambda x: x[idx], reverse=reverse)
-		return items
 	elif isinstance(items, dict):
 		return dict(sorted(items.items(), key=lambda x: x[idx], reverse=reverse))
 
@@ -64,16 +44,16 @@ def lst2dic(lst):
 	return mysort(dic, 1)  # 对字典按值排序
 
 
-def str2dic(string):
+def jsn2dic(jsn):
 	"""
 	将json字符串转为json字典
-	:param string: 待转换字符串
+	:param jsn: 待转换json字符串
 	:return: 转换后的json字典
 	"""
 	dic = {"null": "None", "true": "True", "false": "False"}
 	for d in dic:
-		string = string.replace(d, dic[d])  # 去除特殊字符影响
-	return eval(string)
+		jsn = jsn.replace(d, dic[d])  # 去除特殊字符影响
+	return eval(jsn)
 
 
 def beautify_dic(dic):
@@ -83,8 +63,56 @@ def beautify_dic(dic):
 	:return:
 	"""
 	if isinstance(dic, str):
-		dic = str2dic(dic)
+		dic = jsn2dic(dic)
 	print(json.dumps(dic, indent=4, ensure_ascii=False))
+
+
+def timing():
+	"""
+	统计函数执行时间
+	:return: 装饰器函数
+	"""
+	def decorator(func):  # 装饰器函数
+		def inner(*args):  # 内部函数
+			timer = time.time()  # 计时器
+			datas = func(*args)  # 执行函数
+			print(f"TIMING: {time.time() - timer}")
+			return datas
+		return inner
+	return decorator
+
+
+def loading(timeout):
+	"""
+	多线程打印加载进度条（该函数之后不应再执行其他命令且函数内部不含打印命令）
+	:param timeout: 预计执行时间
+	:return: 装饰器函数
+	"""
+	def sec2str(sec):  # 格式化秒数
+		minute, second = divmod(sec, 60)
+		hour, minute = divmod(minute, 60)
+		return f"{hour:02}:{minute:02}:{second:02}"
+
+	def show_process_bar():  # 显示进度条
+		freq = 4
+		for i in range(timeout * freq + 1):
+			rate = i / (timeout * freq)  # 执行率
+			block = int(rate * 50)  # 实心方块数
+
+			current_time = f"[{sec2str(int(i / freq))}]"  # 已执行时间
+			process_bar = f"{'■' * block}{'□' * (50 - block)}"  # 进度条
+			percentage = f"{rate * 100:>6.2f}%"  # 百分比
+			print(f"\r{current_time} {process_bar} {percentage}", end="")
+			time.sleep(1 / freq)
+
+	def decorator(func):  # 装饰器函数
+		def callback(*args):  # 回调函数
+			thread1 = threading.Thread(target=func, args=args)
+			thread2 = threading.Thread(target=show_process_bar)
+			thread1.start()
+			thread2.start()
+		return callback
+	return decorator
 
 
 def analyze_nlp(kws):
