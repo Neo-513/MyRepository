285bfedad6a3130f77859a3ca6f6319bead7f5a3
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-02-02 14:17:51 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -7,8 +7,7 @@ class BiliBili:
 		self.mc = mycrawler.MyCrawler(cookie=cookie)  # 爬虫
 
 	def get_favs(self, fid):
-		"""
-		爬取收藏夹内容（上限1500）
+		"""爬取收藏夹内容（上限1500）
 		:param fid: 收藏夹id
 		:return: 元素为视频bv、视频标题、up id、up网名的列表
 		"""
@@ -18,8 +17,7 @@ class BiliBili:
 		return [[i["bv_id"], i["title"], i["upper"]["mid"], i["upper"]["name"]] for item in items if item for i in item]
 
 	def get_ups(self, tid=None):
-		"""
-		爬取关注up信息（上限2000）
+		"""爬取关注up信息（上限2000）
 		:param tid: 关注分类id（可选）
 		:return: 键名为up id、键值为up网名的字典
 		"""
@@ -27,3 +25,12 @@ class BiliBili:
 		datas = self.mc.crawl(urls)
 		items = map(lambda x: x["data"]["list"], map(mu.jsn2dic, datas))
 		return {i["mid"]: i["uname"] for item in items for i in item if not tid or (i["tag"] and i["tag"][0] == tid)}
+
+	def get_uname(self, mid):
+		"""根据up id查询up网名
+		:param mid: up id
+		:return: up网名
+		"""
+		url = f"https://api.bilibili.com/x/space/acc/info?mid={mid}"
+		data = self.mc.crawl(url)
+		return mu.jsn2dic(data)["data"]["name"]
