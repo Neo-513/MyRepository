74269c6d9ef9bdb1122cda52f1e3866514a21919
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-02-02 14:22:28 +0800

    Update mycrawler.py

--- mycrawler.py
+++ mycrawler.py
@@ -1,25 +1,28 @@
 import aiofiles
 import aiohttp
 import asyncio
+import json
 import math
 import os
 import re
+import requests
 import time
 
 
 class MyCrawler:
 	HEADERS = {
-		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0."
-		"4664.55 Safari/537.36"
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0."
+		"4692.99 Safari/537.36"
 	}  # 请求头
 
-	def __init__(self, cookie="", encoding="utf-8"):
-		self.HEADERS["cookie"] = cookie
+	def __init__(self, cookie="", encoding="utf-8", show_info=False):
+		if cookie:
+			self.HEADERS["cookie"] = cookie
 		self.encoding = encoding
+		self.show_info = show_info
 
 	def crawl(self, urls, rgx=None, folder=""):
-		"""
-		爬取url列表
+		"""爬取url列表
 		:param urls: url列表（支持单个url）
 		:param rgx: 正则表达式（可选）
 		:param folder: 保存图片目录（可选）
@@ -45,7 +48,13 @@ class MyCrawler:
 		if not folder:
 			if len(urls) == 1:  # 单个url
 				return params[5][urls[0]]
-			else:
+			elif params[4]:  # 含正则搜索
+				datas = []
+				for url in urls:
+					if url in params[5]:
+						datas.extend(params[5][url])  # 按序返回数据
+				return datas
+			else:  # 不含正则搜索
 				return [params[5][url] for url in urls if url in params[5]]  # 按序返回数据
 
 	async def _main(self, tasks, params):  # 任务列表（外部不可调用）
@@ -53,7 +62,9 @@ class MyCrawler:
 		connector = aiohttp.TCPConnector(ssl=False)  # 取消ssl验证
 		async with aiohttp.ClientSession(headers=self.HEADERS, connector=connector) as session:
 			await asyncio.gather(*[asyncio.ensure_future(self._fetch((task, session), params)) for task in tasks])
-		print(f"TIMER: {time.time() - timer}")  # 计时
+		print(f"[CRAWLED IN]    {time.time() - timer}")  # 计时
+		if self.show_info:
+			self._crawler()
 
 	@staticmethod
 	async def _fetch(task, params):  # 爬取单个（外部不可调用）
@@ -73,3 +84,13 @@ class MyCrawler:
 					params[5][url] = params[4].findall(data) if params[4] else data
 			except Exception as e:
 				print(f"{msg}    {e}")  # 打印异常信息
+
+	def _crawler(self):  # 打印爬虫信息字典（外部不可调用）
+		url = "http://httpbin.org/get"
+		with requests.session() as session:
+			session.headers = self.HEADERS
+			response = session.get(url)
+		response.raise_for_status()  # 判断状态码
+		response.encoding = "utf-8"
+		dic = eval(response.text)
+		print(json.dumps(dic, indent=4, ensure_ascii=False))  # 格式化打印
