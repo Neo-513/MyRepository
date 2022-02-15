f88bfde027f69ee9737475ac56cd35c42a0da516
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-02-15 12:56:42 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -4,39 +4,38 @@ import myutils as mu
 
 
 class BiliBili:
-	def __init__(self, cookie):
+	def __init__(self, uid, cookie):
 		self.mc = mycrawler.MyCrawler(cookie=cookie)  # 爬虫
+		self.url_fav = "http://api.bilibili.com/x/v3/fav/resource/list?ps=20"  # 收藏夹url
+		self.url_up = f"http://api.bilibili.com/x/relation/followings?vmid={uid}&order=desc"  # 关注up url
 
 	def get_favs(self, fid):
 		"""爬取收藏夹内容
 		:param fid: 收藏夹id
-		:return: 元素为视频bv、视频标题、up id、up网名的列表
+		:return: 元素为视频标题、up id、up网名的列表
 		"""
-		urls = [f"http://api.bilibili.com/x/v3/fav/resource/list?ps=20&pn={i + 1}&media_id={fid}" for i in range(
-			self._count_favs(fid))]
+		urls = [f"{self.url_fav}&media_id={fid}&pn={i + 1}" for i in range(self._count_fav(fid))]
 		datas = self.mc.crawl(urls)
-		return [[d["bv_id"], d["title"], d["upper"]["mid"], d["upper"]["name"]] for data in datas for d in mu.jsn2dic(
-			data)["data"]["medias"]]
+		return [[d["title"], d["upper"]["mid"], d["upper"]["name"]] for data in datas for d in mu.jsn2dic(data)[
+			"data"]["medias"]]
 
-	def get_ups(self, tag=None):
+	def get_ups(self):
 		"""爬取关注up信息
-		:param tag: 类别
 		:return: 键名为up id、键值为up网名的字典
 		"""
-		urls = [f"http://api.bilibili.com/x/relation/followings?vmid=37928647&order=desc&pn={i + 1}" for i in range(
-			self._count_ups())]
+		urls = [f"{self.url_up}&pn={i + 1}" for i in range(
+			self._count_up())]
 		datas = self.mc.crawl(urls)
-		return {d["mid"]: d["uname"] for data in datas for d in mu.jsn2dic(data)["data"]["list"] if (
-			d["tag"] == [tag] if tag else 1)}
+		return {d["mid"]: d["uname"] for data in datas for d in mu.jsn2dic(data)["data"]["list"]}
 
-	def _count_favs(self, fid):  # 计算收藏夹内容个数（外部不可调用）
-		url = f"http://api.bilibili.com/x/v3/fav/resource/list?ps=20&pn=1&media_id={fid}"
+	def _count_fav(self, fid):  # 计算收藏夹内容个数（外部不可调用）
+		url = f"{self.url_fav}&media_id={fid}&pn=1"
 		data = self.mc.crawl(url)
 		count = mu.jsn2dic(data)["data"]["info"]["media_count"]
 		return math.ceil(count / 20)  # 每页20
 
-	def _count_ups(self):  # 计算关注up个数（外部不可调用）
-		url = f"http://api.bilibili.com/x/relation/followings?vmid=37928647&order=desc&pn=1"
+	def _count_up(self):  # 计算关注up个数（外部不可调用）
+		url = f"{self.url_up}&pn=1"
 		data = self.mc.crawl(url)
 		count = mu.jsn2dic(data)["data"]["total"]
 		return math.ceil(count / 50)  # 每页50
