167d87d2f50af4975e99158bd6c2df7e36c1a6ad
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2022-03-12 15:16:37 +0800

    Update mycrawler.py

--- mycrawler.py
+++ mycrawler.py
@@ -1,96 +1,73 @@
 import aiofiles
 import aiohttp
 import asyncio
-import json
 import math
 import os
 import re
-import requests
 import time
 
+HEADERS = {
+	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0."
+	"4844.51 Safari/537.36"
+}  # 请求头
 
-class MyCrawler:
-	HEADERS = {
-		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0."
-		"4692.99 Safari/537.36"
-	}  # 请求头
 
-	def __init__(self, cookie="", encoding="utf-8", show_info=False):
-		if cookie:
-			self.HEADERS["cookie"] = cookie
-		self.encoding = encoding
-		self.show_info = show_info
+def crawl(urls, cookie="", rgx=None, folder="", encoding="utf-8"):
+	"""url列表进行爬取网页数据或下载图片
+	:param urls: url列表
+	:param cookie: cookie
+	:param rgx: 正则表达式
+	:param folder: 保存图片目录
+	:param encoding: 编码
+	:return: 按url列表顺序的网页数据
+	"""
+	if (not folder and len(urls) > 12000) or (folder and len(urls) > 1000):  # 判断是否异步过载
+		return print("[OVERLOAD]")
+	if cookie:
+		HEADERS["cookie"] = cookie
+	if folder and not os.path.exists(folder):
+		os.mkdir(folder)  # 新建文件夹
 
-	def crawl(self, urls, rgx=None, folder=""):
-		"""爬取url列表
-		:param urls: url列表（支持单个url）
-		:param rgx: 正则表达式（可选）
-		:param folder: 保存图片目录（可选）
-		:return: 按url列表顺序的网页数据
-		"""
-		if isinstance(urls, str):  # 单个url
-			urls = [urls]
-		if (not folder and len(urls) > 12000) or (folder and len(urls) > 1000):  # 判断是否异步过载
-			return print("[OVERLOAD]")
-		if folder and not os.path.exists(folder):
-			os.mkdir(folder)  # 新建文件夹
+	params = {
+		"zfill": math.ceil(math.log(len(urls), 10)),  # 计数器补0位数
+		"folder": folder,  # 文件夹
+		"encoding": encoding,  # 编码
+		"cmpl": re.compile(rgx, re.S) if rgx else None,  # 编译正则表达式
+		"count": -1,  # 计数器
+		"dic": {}  # url字典
+	}
+	asyncio.run(_main(urls, params))  # 异步执行爬取任务列表
 
-		params = (
-			{"count": -1},  # 计数器
-			math.ceil(math.log(len(urls), 10)),  # 计数器补0位数
-			folder,  # 文件夹
-			self.encoding,  # 编码
-			re.compile(rgx, re.S) if rgx else None,  # 编译正则表达式
-			{}  # url字典
-		)
-		asyncio.run(self._main(urls, params))  # 异步执行爬取任务列表
+	if not folder:
+		if params["cmpl"]:  # 含正则搜索
+			datas = []
+			for url in urls:
+				datas.extend(params["dic"][url])  # 返回单个数据列表
+			return datas
+		else:  # 不含正则搜索
+			return [params["dic"][url] for url in urls if url in params["dic"]]  # 按序返回数据
 
-		if not folder:
-			if len(urls) == 1:  # 单个url
-				return params[5][urls[0]]
-			elif params[4]:  # 含正则搜索
-				datas = []
-				for url in urls:
-					if url in params[5]:
-						datas.extend(params[5][url])  # 按序返回数据
-				return datas
-			else:  # 不含正则搜索
-				return [params[5][url] for url in urls if url in params[5]]  # 按序返回数据
 
-	async def _main(self, tasks, params):  # 任务列表（外部不可调用）
-		timer = time.time()  # 计时器
-		connector = aiohttp.TCPConnector(ssl=False)  # 取消ssl验证
-		async with aiohttp.ClientSession(headers=self.HEADERS, connector=connector) as session:
-			await asyncio.gather(*[asyncio.ensure_future(self._fetch((task, session), params)) for task in tasks])
-		print(f"[CRAWLED IN]    {time.time() - timer}")  # 计时
-		if self.show_info:
-			self._crawler()
+async def _main(urls, params):  # 任务列表
+	timer = time.time()  # 计时器
+	connector = aiohttp.TCPConnector(ssl=False)  # 取消ssl验证
+	async with aiohttp.ClientSession(headers=HEADERS, connector=connector) as session:
+		params["session"] = session  # session
+		await asyncio.gather(*[asyncio.ensure_future(_fetch(url, params)) for url in urls])
+	print(f"[CRAWLED IN]    {time.time() - timer}")  # 计时
 
-	@staticmethod
-	async def _fetch(task, params):  # 爬取单个（外部不可调用）
-		url, session = task
-		async with session.get(url) as response:
-			params[0]["count"] += 1  # 计数
-			msg = f"[{params[0]['count']:0{params[1]}}]  {params[2]}  {url}"  # url信息
-			try:
-				print(msg)  # 打印url信息
-				response.raise_for_status()  # 判断页面连接状态
-				if params[2]:  # 爬取图片
-					async with aiofiles.open(f"{params[2]}/{url.split('/')[-1]}", mode="wb") as file:
-						datas = await response.read()
-						await file.write(datas)
-				else:  # 爬取文本
-					data = await response.text(encoding=params[3])
-					params[5][url] = params[4].findall(data) if params[4] else data
-			except Exception as e:
-				print(f"{msg}    {e}")  # 打印异常信息
 
-	def _crawler(self):  # 打印爬虫信息字典（外部不可调用）
-		url = "http://httpbin.org/get"
-		with requests.session() as session:
-			session.headers = self.HEADERS
-			response = session.get(url)
-		response.raise_for_status()  # 判断状态码
-		response.encoding = "utf-8"
-		dic = eval(response.text)
-		print(json.dumps(dic, indent=4, ensure_ascii=False))  # 格式化打印
+async def _fetch(url, params):  # 爬取单个url
+	session, zfill, encoding = params["session"], params["zfill"], params["encoding"]  # 常数参数
+	folder, cmpl = params["folder"], params["cmpl"]  # 可选参数
+	async with session.get(url) as response:
+		params["count"] += 1  # 计数
+		print(f"[{params['count']:0{zfill}}]  {folder}  {url}")  # 打印url信息
+		response.raise_for_status()  # 判断页面连接状态
+		if folder:  # 爬取图片
+			async with aiofiles.open(f"{folder}/{url.split('/')[-1]}", mode="wb") as file:
+				data = await response.read()  # 获取图片内容
+				await file.write(data)  # 写入图片文件
+		else:  # 爬取文本
+			data = await response.text(encoding=encoding)  # 获取文本内容
+			params["dic"][url] = cmpl.findall(data) if cmpl else data  # 加入url字典
